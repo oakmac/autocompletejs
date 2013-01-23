d9edb706815a2344abb49bfc6213d63868056c09
Chris Oakman <chris@oakmac.com>
2013-01-23 10:16:07 -0500

    fix tabs

--- examples/example7.js
+++ examples/example7.js
@@ -2,13 +2,13 @@ var opts = {
   initialList: 'animals',
   lists: {
     animals: [
-	  { value: 'Birds', children: 'birds' },
-	  { value: 'Mammals', children: 'mammals' },
-	  { value: 'Reptiles', children: 'reptiles' }
-	],
-	birds: 'api/birds.php',
-	mammals: 'api/mammals.php',
-	reptiles: 'api/reptiles.php'
+    { value: 'Birds', children: 'birds' },
+    { value: 'Mammals', children: 'mammals' },
+    { value: 'Reptiles', children: 'reptiles' }
+  ],
+  birds: 'api/birds.php',
+  mammals: 'api/mammals.php',
+  reptiles: 'api/reptiles.php'
   }
 };
 var example7 = new AutoComplete('example7', opts);
