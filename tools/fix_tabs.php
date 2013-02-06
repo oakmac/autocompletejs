156fbfecba461109eef3228fa1b390ae724dfcce
Chris Oakman <chris@oakmac.com>
2013-02-06 00:45:11 -0500

    we don't need this stuff

--- tools/fix_tabs.php
+++
@@ -1,8 +0,0 @@
-<?php
-$examples = glob('../examples/*.js');
-foreach ($examples as $file) {
-    exec('expand -i -t2 ' . $file . ' > tmp.js');
-    exec('mv tmp.js ' . $file);
-}
-unlink('tmp.js');
-?>
