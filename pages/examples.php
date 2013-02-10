b0a604b7a63a2dbcdd3e87aa25382daba9e744de
Chris Oakman <chris@oakmac.com>
2013-02-10 18:57:22 -0500

    improve the examples nav

--- pages/examples.php
+++ pages/examples.php
@@ -50,10 +50,24 @@ foreach ($examples as $ex) {
 ?>
 // end examples{}
 
-var showExample = function(number) {
-  $('div#examples_list_container li').removeClass('active');
-  $('li#example_' + number).addClass('active');
+var highlightGroupHeader = function(groupIndex) {
+  $('div#examples_list_container h4').removeClass('active');
+  $('h4#group_header_' + groupIndex).addClass('active');
+};
 
+var highlightExample = function(id) {
+	$('div#examples_list_container li').removeClass('active');
+  $('li#example_' + id).addClass('active');
+};
+
+var showExample = function(number) {
+  var groupIndex = parseInt($('li#example_' + number)
+	  .parent('ul').attr('id').replace('group_container_', ''), 10);
+	
+	$('ul#group_container_' + groupIndex).css('display', '');
+  highlightGroupHeader(groupIndex);
+  highlightExample(number);
+	
   $('#example_name').html(examples[number].name);
   $('#example_single_page_link').attr('href', 'examples/' + number);
   $('#example_html_container').html(examples[number].html);
@@ -65,21 +79,34 @@ var showExample = function(number) {
 var clickExample = function() {
   var number = parseInt($(this).attr('id').replace('example_', ''), 10);
   if (examples.hasOwnProperty(number) !== true)	return;
-  window.location.hash = number;
+  
+	window.location.hash = number;
   loadExampleFromHash();
 };
 
 var loadExampleFromHash = function() {
   var number = parseInt(window.location.hash.replace('#', ''), 10);
   if (examples.hasOwnProperty(number) !== true) {
-  number = 1000;
+    number = 1000;
     window.location.hash = number;
   }
   showExample(number);
 };
 
+var clickGroupHeader = function() {
+  var groupIndex = parseInt($(this).attr('id').replace('group_header_', ''), 10);
+	var examplesEl = $('ul#group_container_' + groupIndex);
+	if (examplesEl.css('display') === 'none') {
+		examplesEl.slideDown('fast');
+	}
+	else {
+		examplesEl.slideUp('fast');
+	}
+};
+
 var init = function() {
   $('#examples_list_container').on('click', 'li', clickExample);
+	$('#examples_list_container').on('click', 'h4', clickGroupHeader);
   loadExampleFromHash();
 };
 $(document).ready(init);
@@ -94,22 +121,25 @@ include(APP_PATH.'pages/footer.php');
 function buildExampleList($examples) {
   $html = '';
   $currentGroup = false;
+	$currentGroupIndex = 0;
   foreach ($examples as $ex) {
 
     // temporary
-	if ($ex['js'] === '') continue;
+	  if ($ex['js'] === '') continue;
 
     if ($ex['group'] !== $currentGroup) {
       if ($currentGroup !== false) {
         $html .= '</ul>'."\n";
       }
       $currentGroup = $ex['group'];
-      $html .= '<h4>'.$currentGroup.'</h4>'."\n";
-      $html .= '<ul>'."\n";
+			$currentGroupIndex++;
+      $html .= '<h4 id="group_header_'.$currentGroupIndex.'">'.$currentGroup.'</h4>'."\n";
+      $html .= '<ul id="group_container_'.$currentGroupIndex.'" style="display:none">'."\n";
     }
 
     $html .= '  <li id="example_'.$ex['number'].'">'.$ex['name'].'</li>'."\n";
   }
+	
   $html .= '</ul>'."\n";
 
   return $html;
