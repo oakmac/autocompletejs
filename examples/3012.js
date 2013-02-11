4b2a2c4f13dd160a61c9e079914f31c3556654fe
Chris Oakman <chris@oakmac.com>
2013-02-11 17:11:50 -0500

    improve example

--- examples/3012.js
+++ examples/3012.js
@@ -1,15 +1,27 @@
-var config = {
+var fruits = [
+  { value: { name: 'Apple', plu: 4015, inSeason: false } },
+  { value: { name: 'Banana', plu: 4011, inSeason: true } },
+  { value: { name: 'Orange', plu: 3037, inSeason: false } }
+];
+
+var widget = new AutoComplete('search_bar', {
   showErrors: 'console',
   lists: {
-    fruits: ['Apple', 'Banana', 'Orange']
+    fruits: {
+      optionHTML: '{name}',
+      options: fruits
+    }
   }
-};
-var widget = new AutoComplete('search_bar', config);
+});
 
-$('#set_value_btn').on('click', function() {
+$('#setValueBtn').on('click', function() {
+  // invalid Token Object because the object does not have
+  // a .tokenHTML property
   widget.setValue({
-    // should be 'tokenHTML'
-    tokenZTML: 'Kiwi',
-    value: 'Kiwi'
+    value: {
+      name: 'Kiwi',
+      plu: 4030,
+      inSeasons: false
+    }
   });
 });
