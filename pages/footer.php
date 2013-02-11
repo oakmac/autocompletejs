9163a4799e26fabe61e5a366d3e3b0a9df088c26
Chris Oakman <chris@oakmac.com>
2013-02-10 23:37:08 -0500

    add footer

--- pages/footer.php
+++ pages/footer.php
@@ -1,4 +1,15 @@
 </div><!-- end div.twelve.columns -->
 </div><!-- end div#body_container -->
+<div id="footer">
+<div class="row">
+  <div class="six columns">
+    <p>Copyright 2013 <a href="mailto:chris@autocompletejs.com">Chris Oakman</a></p>
+    <p>AutoComplete is released under the <a href="https://github.com/oakmac/autocompletejs/blob/master/LICENSE">MIT License</a></p>
+  </div>
+  <div class="six columns">
+    <!-- github link, repeat the nav bar? -->
+  </div>
+</div>
+</div><!-- end div#footer -->
 </body>
 </html>
